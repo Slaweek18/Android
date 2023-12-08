1c679e918a56635c0c9491ceed0180708f6623ce
Slaweek <dnistryan2805@gmail.com>
2023-12-08 13:51:22 +0200

    created 3 screens

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/LoginScreen';
+import HomeScreen from './screens/LoginScreen';
+
+const image = {
+    uri: 'https://i.pinimg.com/564x/26/47/da/2647dacbe4d0f9e91a28da7f8f8dd012.jpg'
+};
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+   image: {
+     flex: 1,
+     justifyContent: 'center',
+   },
 });
